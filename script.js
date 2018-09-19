c707b2b1a9af9cfb79a9ddcfb11cb241dc5bea62
ScottE <scotteratigan@gmail.com>
2018-09-19 13:05:59 -0700

    added .gitignore

--- script.js
+++
@@ -1,7 +0,0 @@
-function increaseFakeCounter(){
-	var fakeCounterObject = document.getElementById("fakeCounter")
-	var fakeCounter = parseInt(fakeCounterObject.innerText)
-	fakeCounter += 1
-	fakeCounterObject.innerText = fakeCounter
-}
-window.addEventListener("mousemove", increaseFakeCounter);
