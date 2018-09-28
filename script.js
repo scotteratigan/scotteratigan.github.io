0ea0c53feb09d3140fe6b327417488260f535882
ScottE <scotteratigan@gmail.com>
2018-09-28 11:05:28 -0700

    made bad page more responsive

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function increaseFakeCounter(){
+function increaseFakeCounter() {
 	var fakeCounterObject = document.getElementById("fakeCounter")
-	var fakeCounter = parseInt(fakeCounterObject.innerText)
-	fakeCounter += 1
-	fakeCounterObject.innerText = fakeCounter
+	var fakeCounter = parseInt(fakeCounterObject.innerText);
+	fakeCounter += 1;
+	if (fakeCounter > 300) {
+		changePage();
+	}
+	fakeCounterObject.innerText = fakeCounter;
 }
+
+function changePage() {
+	window.location="http://home.mcom.com/home/welcome.html";
+}
+
 window.addEventListener("mousemove", increaseFakeCounter);
