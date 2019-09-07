94349d907bf56bbd48a7d8aadfef0df092ebd8d8
ScottE <scotteratigan@gmail.com>
2019-09-06 22:27:07 -0700

    gh-pages not working with node 10

--- publish.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require("gh-pages");
-
-ghpages.publish("build", function(err) {
-  if (err) return console.error("Error encountered when publishing:", err);
-  console.log("Publishing completed.");
-});
