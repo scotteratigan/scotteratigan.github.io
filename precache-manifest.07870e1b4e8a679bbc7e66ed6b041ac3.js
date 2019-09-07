58d98238278c07dd7c88f5b2215802c1e94571a0
ScottE <scotteratigan@gmail.com>
2019-09-06 22:42:15 -0700

    back to original manual deploy - can't publish non-master branch

---
+++ precache-manifest.07870e1b4e8a679bbc7e66ed6b041ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cb6994acfd7c44a949d",
+    "url": "/static/css/main.58255b4a.chunk.css"
+  },
+  {
+    "revision": "5cb6994acfd7c44a949d",
+    "url": "/static/js/main.3afda8de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4a9236b5d6e4c1538e0",
+    "url": "/static/css/2.30c1c968.chunk.css"
+  },
+  {
+    "revision": "d4a9236b5d6e4c1538e0",
+    "url": "/static/js/2.f80a582d.chunk.js"
+  },
+  {
+    "revision": "f3256d0cbeee21469a6b032be6d70b92",
+    "url": "/index.html"
+  }
+];
