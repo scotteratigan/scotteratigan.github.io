54c5f35a45a9ffe97ee4987ef18e45b27ac35eea
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-10-01 11:59:59 -0700

    Updates for new project.

---
+++ precache-manifest.ae52de574f41a65d765161b69e7d5921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48d597af74bc557cfabe3c85d02c224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76f5320c7fa5e9b0e5ea",
+    "url": "/static/css/2.f0678cce.chunk.css"
+  },
+  {
+    "revision": "8e9530bbf4dbc5140f7f",
+    "url": "/static/css/main.eb903427.chunk.css"
+  },
+  {
+    "revision": "76f5320c7fa5e9b0e5ea",
+    "url": "/static/js/2.a00ef01a.chunk.js"
+  },
+  {
+    "revision": "8e9530bbf4dbc5140f7f",
+    "url": "/static/js/main.9c8a5798.chunk.js"
+  },
+  {
+    "revision": "cd3cc059c04efada77a4",
+    "url": "/static/js/runtime-main.336b242f.js"
+  }
+]);
